19dd57c7aa3f5ce43c8b00274a792250ce573d24
Aurélien Bompard <aurelien@bompard.org>
2017-12-18 11:27:03 +0100

    Add more list metadata

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='mailman3-fedmsg-plugin',
-    version='0.5',
+    version='0.6',
     description='Emit fedmsg messages from mailman3',
     author="Ralph Bean",
     author_email="rbean@redhat.com",
